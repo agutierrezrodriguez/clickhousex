66f24a7cec1f4437d3ae5fc3cf58559a816d474c
Antonio Gutiérrez <toniusco@gmail.com>
2020-06-04 15:55:32 +0200

    remove the escape of double quotes

--- lib/clickhousex/codec/values.ex
+++ lib/clickhousex/codec/values.ex
@@ -109,7 +109,7 @@ defmodule Clickhousex.Codec.Values do
     |> String.replace("_", "\_")
     |> String.replace("'", "\'")
     |> String.replace("%", "\%")
-    |> String.replace(~s("), ~s(\\"))
+#    |> String.replace(~s("), ~s(\\"))
     |> String.replace("\\", "\\\\")
   end
 end
